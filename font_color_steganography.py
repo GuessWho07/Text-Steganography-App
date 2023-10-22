991469878168b17a875a7ed376aa8a80f903a8ea
GuessWho07 <paluchjakub2@gmail.com>
2023-10-22 12:56:32 +0200

    Add encoding depth and converting message into bits

--- font_color_steganography.py
+++ font_color_steganography.py
@@ -1,11 +1,21 @@
 from docx import Document
 from math import floor
 
-def covert_string_to_bytes(text):
-    print("TODO: Write function")
+def covert_string_to_bits(text):
+    bits = ""
+    for char in text:
+        ascii_code = ord(char)
+        binary_code = bin(ascii_code)[2:].zfill(8)  # Ensure it's 8 bits long
+        bits += binary_code
+    return bits
 
-def convert_bytes_to_string(bytes):
-    print("TODO: Write function")
+def convert_bits_to_string(bits):
+    result = ""
+    for i in range(0, len(bits), 8):
+        byte = bits[i:i+8]
+        decimal_value = int(byte, 2)
+        result += chr(decimal_value)
+    return result
 
 def calculate_doc_potential(document,depth):
     # Calculate how long message can be hidden in doc
@@ -24,17 +34,46 @@ def calculate_doc_potential(document,depth):
     potential = floor((space_count*depth*3)/8)
     print("In this document, you can hide {0} characters".format(potential))
 
+    return potential
+
 def get_modified_color(original_color, stegano_value, number_of_bytes):
-    print("TODO: Add stegano_value to the original color on the defined number of bytes")
+    print(bin(original_color))
+    print(bin(stegano_value))
+    binary_oc = bin(original_color)[2:number_of_bytes+2]
+    binary_sv = bin(stegano_value)[2:]
+    binary_sv = bin(0)[2:]*(4-len(binary_sv))+binary_sv
+    print("COLORS: ",binary_oc, binary_sv)
+    print("NEW_COLOR: {0}{1}".format(binary_oc,binary_sv))
+
+    return binary_oc+binary_sv
 
 def hide_message(message,document,depth):
     # Zamień message na ciąg bitów
-    # Zdefiniuj na ilu bitach przechowujemy wiadomość
-    # Zakoduj to na ostatnich 3 bitach pierwszej spacji
+    message_bytes = covert_string_to_bits(message)
+    print("Encoded message: ",message_bytes)
+    
+    # Zakoduj to na ostatnich 4 bitach koloru R pierwszej spacji
+    binary_depth = bin(depth)[2:]
+    print(binary_depth)
+    print(get_modified_color(255,depth,4))
+    #TODO: Faktycznie zmień kolor spacji, wgle trzeba jakoś trackować spacje które zmieniamy i ich wartości RGB
+
     # Podziel strumień bitów na supergrupy po 3 grupy po X bitów, ew. dodaj padding
-    # Weź spację, weź jej kolor, dodaj do niego tajną wartość
+    divided_message = []
+    for i in range(0, len(message_bytes),3*depth):
+        divided_message.append(message_bytes[i:i+(3*depth)])
+
+    print(divided_message)
+    print(convert_bits_to_string(''.join(divided_message)))
+
+    for i in range(len(divided_message)):
+        # Weź spację
+        # Podziel supergrupę na 3
+        # Zmień wartości RGB
+        continue
+
+    # Weź spację, weź jej kolor, dodaj do niego tajną wartość / Na razie zakładamy że jest czarna
     # ^^^ Powtórz ile razy trzeba
-    # Dodaj na początku 3 bitową wartość, definiującą na ilu bitach chowamy wiadomość
     pass
 
 def show_message(document):
@@ -47,20 +86,28 @@ def main():
         action = int(input("1 - Hide message in document\n2 - Extract message from document\n Your choice...: "))
         if action == 1:
             full_path = input("Please, provide full PATH to the Document you want to hide message in...\n")
+            full_path="C:\\"+full_path
 
             while True:
-                depth = int(input("Please provide depth of the message (range from 1 to 8). Lower values will make the font color less distinghushible from color black, but also less characters will be coded within one space\nYour choice (1-8): "))
-                if depth in range (1,9):
-                    break
-                else:
-                    print("Invalid value, must be in range (1,8)")
+                while True:
+                    depth = int(input("Please provide depth of the message (range from 1 to 8). Lower values will make the font color less distinghushible from color black, but also less characters will be coded within one space\nYour choice (1-8): "))
+                    if depth in range (1,9):
+                        break
+                    else:
+                        print("Invalid value, must be in range (1,8)")
+                        continue
+                
+                # Calculate max length of message that can be hidden in this document
+                document = Document(full_path)
+                potential = calculate_doc_potential(document,depth)
+                message = input("Please, provide message you want to hide in your document\nMessage: ")
+                if len(message)>potential:
+                    print("Your message is too long for your doc, please pick other parameters...")
                     continue
+                else:
+                    break
             
-            # Calculate max length of message that can be hidden in this document
-            document = Document(full_path)
-            calculate_doc_potential(document,depth)
-            message = input("Please, provide message you want to hide in your document\nMessage: ")
-
+            hide_message(message,document,depth)
 
         elif action == 2:
             full_path = input("Please, provide full PATH to the Document you want to extract message from...\n")
